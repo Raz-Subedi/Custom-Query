eee6852f07a78ff491186ac87a1d3eab16ad9e9c
Raz Subedi <subedirazkumar@gmail.com>
2025-01-04 20:59:02 +0545

    Entity

---
+++ src/test/java/com/raz/Custom_Query/CustomQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raz.Custom_Query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
